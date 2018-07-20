df9683eda91289a99eb81df2c2bd1553d9c5a530
darkobits <joshua@frontlawn.net>
2018-07-19 18:58:07 -0700

    chore: Add boilerplate.

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const ALWAYS_IGNORE = [
+  '<rootDir>/dist',
+  '/node_modules/'
+];
+
+const EXTENSIONS = ['ts', 'tsx', 'js', 'jsx', 'node'];
+
+module.exports = {
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ALWAYS_IGNORE,
+  coveragePathIgnorePatterns: ALWAYS_IGNORE,
+  testRegex: `^.+\\.spec.(${EXTENSIONS.join('|')})$`,
+  moduleFileExtensions: EXTENSIONS,
+  transform: {
+    [`^.+\\.(${EXTENSIONS.join('|')})$`]: 'babel-jest'
+  }
+};
